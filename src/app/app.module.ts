aa4ea176126ba7f958a2e9a2cc434485fb26d56b
“brenda-mareco” <brendaamareco@gmail.com>
2023-02-18 14:17:31 -0300

    Created modules and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
+import { PortfolioModule } from './modules/portfolio/portfolio.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    PortfolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]

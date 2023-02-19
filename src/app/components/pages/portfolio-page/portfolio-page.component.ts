adee2e5331efe6d66ebc2258385d6053cbdb8a9a
“brenda-mareco” <brendaamareco@gmail.com>
2023-02-18 21:57:41 -0300

    Added portfolio page

---
+++ src/app/components/pages/portfolio-page/portfolio-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-page',
+  templateUrl: './portfolio-page.component.html',
+  styleUrls: ['./portfolio-page.component.scss']
+})
+export class PortfolioPageComponent {
+
+}
--- src/app/modules/portfolio/portfolio.module.ts
+++ src/app/modules/portfolio/portfolio.module.ts
@@ -5,11 +5,13 @@ import { NavComponent } from 'src/app/components/nav/nav.component';
 import { MaterialModule } from '../material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PortfolioLayoutComponent } from 'src/app/components/portfolio-layout/portfolio-layout.component';
+import { PortfolioPageComponent } from 'src/app/components/pages/portfolio-page/portfolio-page.component';
 
 @NgModule({
   declarations: [
     NavComponent,
-    PortfolioLayoutComponent
+    PortfolioLayoutComponent,
+    PortfolioPageComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +21,8 @@ import { PortfolioLayoutComponent } from 'src/app/components/portfolio-layout/po
   ],
   exports: [
     NavComponent,
-    PortfolioLayoutComponent
+    PortfolioLayoutComponent,
+    PortfolioPageComponent
   ]
 })
 export class PortfolioModule { }

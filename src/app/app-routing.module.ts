509d2771710ab61c11d8b6bb759f2e9f2b180888
“brenda-mareco” <brendaamareco@gmail.com>
2023-02-18 22:00:51 -0300

    Added routing to portfolio page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioPageComponent } from './components/pages/portfolio-page/portfolio-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: PortfolioPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

22d0d94e08b2f081f0de6797508264e099c169c4
“brenda-mareco” <brendaamareco@gmail.com>
2023-03-25 20:34:23 -0300

    Added data binding to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Profile } from 'src/app/models/profile.interface';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  profile: Profile | undefined;
+
+  constructor(private profileService: ProfileService){}
+
+  ngOnInit(): void {
+    this.profileService.getProfile(1)?.subscribe( profile => this.profile = profile);
+  }
+
 
 }

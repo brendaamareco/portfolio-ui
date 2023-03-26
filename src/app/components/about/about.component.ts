f7916d93d2dd3c1fbd4743daab223b11c8619587
“brenda-mareco” <brendaamareco@gmail.com>
2023-03-25 21:02:38 -0300

    Added profile service to about component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Profile, Owner } from 'src/app/models/profile.interface';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  profile: Profile | undefined;
+  owner: Owner | undefined;
+  
+  constructor(private profileService: ProfileService){}
+
+  ngOnInit(): void {
+    this.profileService.getProfile(1).subscribe(
+      profile => { this.profile = profile;
+                    this.owner = profile.owner }
+    )
+  }
 
 }

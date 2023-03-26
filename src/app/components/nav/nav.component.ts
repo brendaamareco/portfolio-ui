87152abdd712a9e8f30bb0af139d384f3cfb6e6e
“brenda-mareco” <brendaamareco@gmail.com>
2023-03-25 21:00:05 -0300

    Added portfolio service to nav component

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Owner } from 'src/app/models/profile.interface';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  owner: Owner | undefined;
+  
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this
+    .profileService.getProfile(1)
+    .subscribe( profile => { this.owner = profile.owner; });
+  }
 
 }

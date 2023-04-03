a4d2d48923d6d14274b38d850d010828dd23fe91
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-03 18:25:25 -0300

    Merged owner and profile interface into owner

--- src/app/models/owner.interface.ts
+++ src/app/models/owner.interface.ts
@@ -4,4 +4,8 @@ export interface Owner {
     lastName: string;
     country:  string;
     province: string;
+    thumbnail:   string;
+    welcomeText: string;
+    description: string;
+    role:        string;
 }
--- src/app/models/project.interface.ts
+++ src/app/models/project.interface.ts
@@ -1,9 +1,6 @@
-import { Owner } from "./owner.interface";
-
 export interface Project {
     id?:         number;
     title:       string;
     description: string;
     thumbnail:   string;
-    owner:       Owner;
 }

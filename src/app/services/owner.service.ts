6a34d62b37b26bc03c0b0ef0b77b6194d4982d02
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-03 18:29:22 -0300

    Added get and uptade method in owner service

--- src/app/services/owner.service.ts
+++ src/app/services/owner.service.ts
@@ -1,10 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Owner } from '../models/owner.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OwnerService {
 
-  constructor(httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
+  
+  get(): Observable<Owner>
+  {
+    return this.http.get<Owner>("http://localhost:8080/api/owner")
+  }
+
+  update(owner: Owner): Observable<Owner>
+  {
+    return this.http.put<Owner>("http://localhost:8080/api/owner", owner);
+  }
 }

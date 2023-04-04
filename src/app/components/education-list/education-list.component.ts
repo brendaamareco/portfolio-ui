12b286064c1d23978e4d3552cf613d6d57cf1f9d
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-04 17:28:52 -0300

    Loaded education list

--- src/app/components/education-list/education-list.component.ts
+++ src/app/components/education-list/education-list.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Education } from 'src/app/models/education.interface';
+import { EducationService } from 'src/app/services/education.service';
 
 @Component({
   selector: 'app-education-list',
   templateUrl: './education-list.component.html',
   styleUrls: ['./education-list.component.scss']
 })
-export class EducationListComponent {
+export class EducationListComponent 
+{
+  educationList: Education[] = [];
 
+  constructor(private educationService: EducationService) 
+  {
+    this.loadEducationList();
+  }
+
+  loadEducationList(): void
+  {
+    this.educationService
+    .getEducationList()
+    .subscribe( educationList => this.educationList = educationList);
+  }
 }

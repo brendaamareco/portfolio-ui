b790141512f66d4109fd8dd9de8a7c8c75a7d1f6
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-04 16:30:38 -0300

    Designed project delete dialog component

--- src/app/components/project/project-delete-dialog/project-delete-dialog.component.ts
+++ src/app/components/project/project-delete-dialog/project-delete-dialog.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Project } from 'src/app/models/project.interface';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-project-delete-dialog',
   templateUrl: './project-delete-dialog.component.html',
   styleUrls: ['./project-delete-dialog.component.scss']
 })
-export class ProjectDeleteDialogComponent {
+export class ProjectDeleteDialogComponent 
+{
+  projectTitle: string = "";
 
+  constructor(public dialog: MatDialogRef<ProjectDeleteDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private project: Project, private projectService: ProjectService) 
+    {
+      this.projectTitle = project.title;
+    }
+
+  deleteProject(): void
+  {
+    console.log("Project deleted");
+  }
 }
--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -26,6 +26,9 @@ export class ProjectComponent
 
   openDeleteDialog(): void
   {
-    this.dialog.open(ProjectDeleteDialogComponent);
+    this.dialog
+    .open(ProjectDeleteDialogComponent, { data: this.project })
+    .afterClosed()
+    .subscribe(() => this.projectEditedEvent.emit());
   }
 }

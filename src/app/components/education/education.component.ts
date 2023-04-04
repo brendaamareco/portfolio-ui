0794a4da60dd7b3f0c938edb9cdb4a825bca084b
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-04 17:31:34 -0300

    Transferred education data from parent to child

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Education } from 'src/app/models/education.interface';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.scss']
 })
-export class EducationComponent {
-
+export class EducationComponent 
+{
+  @Input() education: Education | undefined;
 }

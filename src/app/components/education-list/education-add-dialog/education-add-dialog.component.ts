61619ea381595015def0697643792dba5a87a8ea
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-05 16:11:41 -0300

    Created add dialog form

--- src/app/components/education-list/education-add-dialog/education-add-dialog.component.ts
+++ src/app/components/education-list/education-add-dialog/education-add-dialog.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { EducationService } from 'src/app/services/education.service';
 
 @Component({
   selector: 'app-education-add-dialog',
   templateUrl: './education-add-dialog.component.html',
   styleUrls: ['./education-add-dialog.component.scss']
 })
-export class EducationAddDialogComponent {
+export class EducationAddDialogComponent 
+{
+  educationAddForm: FormGroup = new FormGroup({});
+
+  constructor(public dialogRef: MatDialogRef<EducationAddDialogComponent>, private formBuilder: FormBuilder, private educationService: EducationService) 
+  {
+    this.educationAddForm = formBuilder.group({
+      institution: ['', Validators.required],
+      title: ['', Validators.required],
+      description: [''],
+      thumbnail: [''],
+      startDate: [Date.now(), Validators.required],
+      endDate: [Date.now(), Validators.required]
+    })
+  }
+
+
 
 }

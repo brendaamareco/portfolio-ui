00d1fcfe1ae70ce874f718e94e6d814f9b516411
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-06 14:57:00 -0300

    Imported and added datepicker method for education edit form

--- src/app/components/education/education-edit-dialog/education-edit-dialog.component.ts
+++ src/app/components/education/education-edit-dialog/education-edit-dialog.component.ts
@@ -1,14 +1,42 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import moment from 'moment';
 import { Education } from 'src/app/models/education.interface';
 import { EducationService } from 'src/app/services/education.service';
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MatDatepicker} from '@angular/material/datepicker';
+
+import * as _moment from 'moment';
+import {default as _rollupMoment, Moment} from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-education-edit-dialog',
   templateUrl: './education-edit-dialog.component.html',
-  styleUrls: ['./education-edit-dialog.component.scss']
+  styleUrls: ['./education-edit-dialog.component.scss'],
+  providers: [ 
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS}
+  ],
+  encapsulation: ViewEncapsulation.None
 })
 export class EducationEditDialogComponent 
 {
@@ -55,5 +83,14 @@ export class EducationEditDialogComponent
   get endDate()
   {
     return this.educationEditForm.get('endDate') as FormControl;
+  }
+
+  setMonthAndYear(normalizedMonthAndYear: Moment, datepicker: MatDatepicker<Moment>, formControl: FormControl) 
+  {
+    const ctrlValue = formControl.value;
+    ctrlValue.month(normalizedMonthAndYear.month());
+    ctrlValue.year(normalizedMonthAndYear.year());
+    formControl.setValue(ctrlValue);
+    datepicker.close();
   }
 }

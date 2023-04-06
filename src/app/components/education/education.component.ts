d458ed07f4df64f438f211e409fbf0107bd25814
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-06 14:47:11 -0300

    Implemented open education edit dialog

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Education } from 'src/app/models/education.interface';
+import { EducationEditDialogComponent } from './education-edit-dialog/education-edit-dialog.component';
 
 @Component({
   selector: 'app-education',
@@ -9,4 +11,15 @@ import { Education } from 'src/app/models/education.interface';
 export class EducationComponent 
 {
   @Input() education: Education | undefined;
+  @Output() educationEdited: EventEmitter<void> = new EventEmitter();
+
+  constructor(public dialog: MatDialog) {}
+
+  openEditDialog(): void
+  {
+    this.dialog
+    .open(EducationEditDialogComponent, { data: this.education })
+    .afterClosed()
+    .subscribe(() => { this.educationEdited.emit() });
+  }
 }

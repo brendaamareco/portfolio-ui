094f06eae8d1544e15ad2e3f54f6beb6f974b2b9
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-07 14:14:04 -0300

    Added experienceService dependency to experienceListComponent

--- src/app/components/experience-list/experience-list.component.ts
+++ src/app/components/experience-list/experience-list.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Experience } from 'src/app/models/experience.interface';
+import { ExperienceService } from 'src/app/services/experience.service';
 
 @Component({
   selector: 'app-experience-list',
   templateUrl: './experience-list.component.html',
   styleUrls: ['./experience-list.component.scss']
 })
-export class ExperienceListComponent {
+export class ExperienceListComponent 
+{
+  experienceList: Experience[] = [];
 
+  constructor(private experienceService: ExperienceService) 
+  {
+    this.loadExperienceList();
+  }
+
+  loadExperienceList(): void
+  {
+    this.experienceService
+    .getExperienceList()
+    .subscribe( experienceList => { this.experienceList = experienceList });
+  }
 }

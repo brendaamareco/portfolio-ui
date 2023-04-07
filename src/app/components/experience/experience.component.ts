a136abdd60c08a441cc45172d3b631a658e0e223
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-07 14:25:07 -0300

    Binded experience data from parent

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Experience } from 'src/app/models/experience.interface';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
-export class ExperienceComponent {
-
+export class ExperienceComponent 
+{
+  @Input() experience: Experience | undefined;
 }

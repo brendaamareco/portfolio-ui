2d378c37b5a0a877280e79bc9b22b8df8fcf4592
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-07 14:10:53 -0300

    Implemented getExperienceList() in ExperienceService

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -1,11 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Experience } from '../models/experience.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperienceService {
 
+  rootUrl: string = "http://localhost:8080/api/";
+
   constructor(private httpClient: HttpClient) { }
+
+  getExperienceList(): Observable<Experience[]>
+  {
+    return this.httpClient.get<Experience[]>(this.rootUrl + "experience");
+  }
 
 }

b9d1d527b8e3ed322bdb5c0c5e55d36eddce047e
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-07 14:10:19 -0300

    Created experience model

---
+++ src/app/models/experience.interface.ts
@@ -0,0 +1,9 @@
+export interface Experience {
+    id?:         number;
+    title:       string;
+    description: string;
+    thumbnail:   string;
+    startDate:   Date;
+    endDate:     Date;
+    companyName: string;
+}

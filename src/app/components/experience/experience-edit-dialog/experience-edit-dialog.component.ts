628d333bd5aaa2b2a9ac500ec27e02545673071e
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-10 16:05:21 -0300

    Created reactive form in experience edit dialog component

--- src/app/components/experience/experience-edit-dialog/experience-edit-dialog.component.ts
+++ src/app/components/experience/experience-edit-dialog/experience-edit-dialog.component.ts
@@ -1,10 +1,116 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Experience } from 'src/app/models/experience.interface';
+import { ExperienceService } from 'src/app/services/experience.service';
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MatDatepicker} from '@angular/material/datepicker';
+
+import * as _moment from 'moment';
+import {default as _rollupMoment, Moment} from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-experience-edit-dialog',
   templateUrl: './experience-edit-dialog.component.html',
-  styleUrls: ['./experience-edit-dialog.component.scss']
+  styleUrls: ['./experience-edit-dialog.component.scss'],
+  providers: [ 
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS}
+  ],
+  encapsulation: ViewEncapsulation.None
 })
-export class ExperienceEditDialogComponent {
+export class ExperienceEditDialogComponent 
+{
 
+  editExperienceForm: FormGroup = new FormGroup({});
+
+  constructor(public dialogRef: MatDialogRef<ExperienceEditDialogComponent>, @Inject(MAT_DIALOG_DATA) private experience: Experience, private experienceService: ExperienceService, private formBuilder: FormBuilder)
+  {
+    this.editExperienceForm = formBuilder.group(
+      {
+        title: [this.experience.title, Validators.required],
+        description: [this.experience.description],
+        thumbnail: [this.experience.thumbnail],
+        startDate: [this.experience.startDate, Validators.required],
+        endDate: [this.experience.endDate, Validators.required],
+        companyName: [this.experience.companyName, Validators.required]
+      }
+    );
+  }
+
+  submit(): void
+    {
+      this.experienceService
+      .updateExperience(
+        {
+          id: this.experience.id,
+          title: this.title.value,
+          description: this.description.value,
+          thumbnail: this.thumbnail.value,
+          startDate: this.startDate.value,
+          endDate: this.endDate.value,
+          companyName: this.companyName.value
+        })
+      .subscribe( () => {});
+
+      this.dialogRef.close();
+    }
+
+  setMonthAndYear(normalizedMonthAndYear: Moment, datepicker: MatDatepicker<Moment>, formControl: FormControl) 
+    {
+      const ctrlValue = formControl.value;
+      ctrlValue.month(normalizedMonthAndYear.month());
+      ctrlValue.year(normalizedMonthAndYear.year());
+      formControl.setValue(ctrlValue);
+      datepicker.close();
+    }
+
+  get title()
+    {
+      return this.editExperienceForm.get('title') as FormControl;
+    }
+
+    get description()
+    {
+      return this.editExperienceForm.get('description') as FormControl;
+    }
+
+    get thumbnail()
+    {
+      return this.editExperienceForm.get('thumbnail') as FormControl;
+    }
+
+    get startDate()
+    {
+      return this.editExperienceForm.get('startDate') as FormControl;
+    }
+
+    get endDate()
+    {
+      return this.editExperienceForm.get('endDate') as FormControl;
+    }
+
+    get companyName()
+    {
+      return this.editExperienceForm.get('companyName') as FormControl;
+    }
 }
--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Experience } from 'src/app/models/experience.interface';
+import { ExperienceEditDialogComponent } from './experience-edit-dialog/experience-edit-dialog.component';
 
 @Component({
   selector: 'app-experience',
@@ -9,4 +11,15 @@ import { Experience } from 'src/app/models/experience.interface';
 export class ExperienceComponent 
 {
   @Input() experience: Experience | undefined;
+  @Output() experienceChanged: EventEmitter<void> = new EventEmitter();
+
+  constructor(public dialog: MatDialog) {}
+
+  openEditDialog(): void
+  {
+    this.dialog
+    .open(ExperienceEditDialogComponent, { data: this.experience })
+    .afterClosed()
+    .subscribe(() => { this.experienceChanged.emit(); });
+  }
 }

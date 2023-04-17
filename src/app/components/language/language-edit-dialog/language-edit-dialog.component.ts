5b14ec42ab39ded3169cb801de0c43bc2119d597
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-17 16:34:43 -0300

    Finished edit language dialog

--- src/app/components/language/language-edit-dialog/language-edit-dialog.component.ts
+++ src/app/components/language/language-edit-dialog/language-edit-dialog.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Language, LanguageLevel } from 'src/app/models/language.interface';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-language-edit-dialog',
   templateUrl: './language-edit-dialog.component.html',
   styleUrls: ['./language-edit-dialog.component.scss']
 })
-export class LanguageEditDialogComponent {
+export class LanguageEditDialogComponent 
+{
+  editLanguageForm: FormGroup = new FormGroup({});
+  levels = Object.values(LanguageLevel).filter((v) => isNaN(Number(v)));
 
+  constructor(private dialogRef: MatDialogRef<LanguageEditDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private language: Language,
+    private formBuilder: FormBuilder, private languageService: LanguageService)
+  {
+    this.editLanguageForm = formBuilder.group(
+      {
+        name: [this.language.name, Validators.compose([Validators.required, Validators.maxLength(45)])],
+        level: [LanguageLevel[this.language.level], Validators.compose([Validators.required])]
+      }
+    );
+  }
+
+  get name(): FormControl
+  {
+    return this.editLanguageForm.get('name') as FormControl;
+  }
+
+  get level(): FormControl
+  {
+    return this.editLanguageForm.get('level') as FormControl;
+  }
+
+  submit(): void
+  {
+    this.languageService.update(
+      {
+        id: this.language.id,
+        name: this.name.value,
+        level: this.level.value
+      }
+    ).subscribe( () => {} );
+
+    this.dialogRef.close();
+  }
 }
--- src/app/components/language/language.component.ts
+++ src/app/components/language/language.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Language, LanguageLevel } from 'src/app/models/language.interface';
+import { LanguageEditDialogComponent } from './language-edit-dialog/language-edit-dialog.component';
 
 @Component({
   selector: 'app-language',
@@ -12,8 +14,18 @@ export class LanguageComponent
   @Output() languageChanged = new EventEmitter<void>();
   levelValue: number = this.getLevelValue();
 
+  constructor(private dialog: MatDialog){}
+
   getLevelValue(): number {
     let levelValue = LanguageLevel[this.language.level] as unknown as number;
     return levelValue*20;
+  }
+
+  openEditDialog(): void 
+  {
+    this.dialog
+    .open(LanguageEditDialogComponent, { data: this.language })
+    .afterClosed()
+    .subscribe( () => this.languageChanged.emit() );
   }
 }

8e279b574ddf628791061981582ba0397d9a4167
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-16 22:34:27 -0300

    Created reactive edit hardskill form

--- src/app/components/hard-skill/hard-skill-edit-dialog/hard-skill-edit-dialog.component.ts
+++ src/app/components/hard-skill/hard-skill-edit-dialog/hard-skill-edit-dialog.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { HardSkill } from 'src/app/models/hardSkill.interface';
+import { HardSkillService } from 'src/app/services/hard-skill.service';
 
 @Component({
   selector: 'app-hard-skill-edit-dialog',
   templateUrl: './hard-skill-edit-dialog.component.html',
   styleUrls: ['./hard-skill-edit-dialog.component.scss']
 })
-export class HardSkillEditDialogComponent {
+export class HardSkillEditDialogComponent 
+{
 
+  editHardSkillForm: FormGroup = new FormGroup({});
+
+  constructor(private dialogRef: MatDialogRef<HardSkillEditDialogComponent>, @Inject(MAT_DIALOG_DATA) private hardSkill: HardSkill, private formBuilder: FormBuilder, private hardSkillService: HardSkillService) 
+  {
+    this.editHardSkillForm = formBuilder.group(
+      {
+        name: [this.hardSkill.name, Validators.compose([Validators.required, Validators.maxLength(45)])],
+        level: [this.hardSkill.level, Validators.compose([Validators.required, Validators.min(0), Validators.max(100)])]
+      });
+  }
+
+  get name(): FormControl
+  {
+    return this.editHardSkillForm.get('name') as FormControl;
+  }
+
+  get level(): FormControl
+  {
+    return this.editHardSkillForm.get('level') as FormControl;
+  }
+
+  submit(): void
+  {
+    this.hardSkillService.update({
+      id: this.hardSkill.id,
+      name: this.name.value,
+      level: this.level.value
+    }).subscribe( () => {} )
+
+    this.dialogRef.close();
+  }
 }
--- src/app/components/hard-skill/hard-skill.component.ts
+++ src/app/components/hard-skill/hard-skill.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { HardSkill } from 'src/app/models/hardSkill.interface';
+import { HardSkillEditDialogComponent } from './hard-skill-edit-dialog/hard-skill-edit-dialog.component';
 
 @Component({
   selector: 'app-hard-skill',
@@ -10,4 +12,14 @@ export class HardSkillComponent
 {
   @Input() hardSkill: HardSkill | undefined;
   @Output() hardSkillChanged = new EventEmitter<void>();
+
+  constructor(public dialog: MatDialog){}
+
+  openEditDialog(): void
+  {
+    this.dialog
+    .open(HardSkillEditDialogComponent, { data: this.hardSkill })
+    .afterClosed()
+    .subscribe(() => { this.hardSkillChanged.emit() });
+  }
 }

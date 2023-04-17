b4f9641e93919b657cd780de8fa209e9404df481
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-17 15:36:18 -0300

    Binded language data

--- src/app/components/language-list/language-list.component.ts
+++ src/app/components/language-list/language-list.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Language } from 'src/app/models/language.interface';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-language-list',
   templateUrl: './language-list.component.html',
   styleUrls: ['./language-list.component.scss']
 })
-export class LanguageListComponent {
+export class LanguageListComponent 
+{
+  languageList: Language[] = [];
 
+  constructor(private languageService: LanguageService)
+  {
+    this.loadLanguageList();
+  }
+
+  loadLanguageList(): void
+  {
+    this.languageService
+    .getAll()
+    .subscribe( (languageList) => this.languageList = languageList );
+  }
 }
--- src/app/components/language/language.component.ts
+++ src/app/components/language/language.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Language, LanguageLevel } from 'src/app/models/language.interface';
 
 @Component({
   selector: 'app-language',
   templateUrl: './language.component.html',
   styleUrls: ['./language.component.scss']
 })
-export class LanguageComponent {
+export class LanguageComponent 
+{
+  @Input() language: Language = { name: '', level: LanguageLevel.A1 };
+  @Output() languageChanged = new EventEmitter<void>();
+  levelValue: number = this.getLevelValue();
 
+  getLevelValue(): number {
+    let levelValue = LanguageLevel[this.language.level] as unknown as number;
+    return levelValue*20;
+  }
 }

3fe757858c459db0f5150841ea28627200a693cf
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-16 22:00:36 -0300

    Binded hard skill data

--- src/app/components/hard-skill-list/hard-skill-list.component.ts
+++ src/app/components/hard-skill-list/hard-skill-list.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HardSkill } from 'src/app/models/hardSkill.interface';
+import { HardSkillService } from 'src/app/services/hard-skill.service';
 
 @Component({
   selector: 'app-hard-skill-list',
   templateUrl: './hard-skill-list.component.html',
   styleUrls: ['./hard-skill-list.component.scss']
 })
-export class HardSkillListComponent {
+export class HardSkillListComponent 
+{
+  hardSkills: HardSkill[] = [];
 
+  constructor(private hardSkillService: HardSkillService) 
+  {
+    this.loadHardSkills();
+  }
+
+  loadHardSkills(): void
+  {
+    this.hardSkillService
+    .getAll()
+    .subscribe( (hardSkills) => this.hardSkills = hardSkills )
+  }
 }
--- src/app/components/hard-skill/hard-skill.component.ts
+++ src/app/components/hard-skill/hard-skill.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { HardSkill } from 'src/app/models/hardSkill.interface';
 
 @Component({
   selector: 'app-hard-skill',
   templateUrl: './hard-skill.component.html',
   styleUrls: ['./hard-skill.component.scss']
 })
-export class HardSkillComponent {
-
+export class HardSkillComponent 
+{
+  @Input() hardSkill: HardSkill | undefined;
+  @Output() hardSkillChanged = new EventEmitter<void>();
 }

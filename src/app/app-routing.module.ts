aa5228402f629e7fc8aec8d61f60545717ea54a5
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-20 19:33:59 -0300

    Created login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: PortfolioPageComponent
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginPageComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortfolioModule } from './modules/portfolio/portfolio.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,

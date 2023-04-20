3558aea434c692af7f06d96b01d07f66c36acd45
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-20 14:45:23 -0300

    Added validations to project edit form

--- src/app/components/project/project-edit-dialog/project-edit-dialog.component.ts
+++ src/app/components/project/project-edit-dialog/project-edit-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Project } from 'src/app/models/project.interface';
 import { ProjectService } from 'src/app/services/project.service';
@@ -9,8 +9,8 @@ import { ProjectService } from 'src/app/services/project.service';
   templateUrl: './project-edit-dialog.component.html',
   styleUrls: ['./project-edit-dialog.component.scss']
 })
-export class ProjectEditDialogComponent {
-
+export class ProjectEditDialogComponent 
+{
   projectEditForm: FormGroup = new FormGroup({});
 
   constructor(public dialogRef: MatDialogRef<ProjectEditDialogComponent>,
@@ -18,17 +18,24 @@ export class ProjectEditDialogComponent {
     {
       this.projectEditForm = formBuilder.group(
         {
-          title: [this.project.title, Validators.compose([Validators.required, Validators.maxLength(32)])],
-          description: [this.project.description],
-          thumbnail: [this.project.thumbnail],
-          projectUrl: [this.project.projectUrl]
+          title: [this.project.title, Validators.compose([Validators.required, Validators.maxLength(64)])],
+          description: [this.project.description, Validators.maxLength(1024)],
+          thumbnail: [this.project.thumbnail, Validators.maxLength(2048)],
+          projectUrl: [this.project.projectUrl, Validators.maxLength(2048)]
         });
     }
 
-    get title() { return this.projectEditForm.get("title"); }
-    get description() { return this.projectEditForm.get("description"); }
-    get thumbnail() { return this.projectEditForm.get("thumbnail"); }
-    get projectUrl() { return this.projectEditForm.get("projectUrl"); }    
+    get title(): FormControl 
+    { return this.projectEditForm.get("title") as FormControl; }
+
+    get description(): FormControl 
+    { return this.projectEditForm.get("description") as FormControl; }
+
+    get thumbnail(): FormControl 
+    { return this.projectEditForm.get("thumbnail") as FormControl; }
+
+    get projectUrl(): FormControl 
+    { return this.projectEditForm.get("projectUrl") as FormControl; }    
     
     submitProject()
     {
